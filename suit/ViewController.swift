52cb379aa9660be1940a27e11ff75a24e3a6dacd
Rizki Dwi Putra <rizki.jtk07@gmail.com>
2020-05-11 12:08:16 +0700

    Initial Commit

---
+++ suit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  suit
+//
+//  Created by Rizki Dwi Putra on 11/05/20.
+//  Copyright © 2020 Rizki Dwi Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
